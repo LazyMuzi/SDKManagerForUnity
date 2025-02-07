d1c92cb42698355dc252e8ee6d078b8bfc0c3d61
bello <tkdgns0470@gmail.com>
2025-02-07 22:55:57 +0900

    feat: add packages to sdk manager (one push, mobile native popup, event popup, purchase popup, i2)

--- Assets/Scripts/PathConstant.cs
+++ Assets/Scripts/PathConstant.cs
@@ -6,5 +6,19 @@ public class PathConstant
 {
     public static string MembershipURL = "git@github.com:pinkfong-tech/com.thepinkfongcompany.core.membership.git";
     public static string MembershipPath = "MyPackages/Membership";
-
+        
+    public static string OnePushURL = "git@github.com:pinkfong-tech/com.thepinkfongcompany.push.onepush.git";
+    public static string OnePushPath = "MyPackages/OnePush";
+    
+    public static string MobileNativePopupURL = "git@github.com:pinkfong-tech/com.thepinkfongcompany.ui.mobile-native-popups.git";
+    public static string MobileNativePopupPath = "MyPackages/MobileNativePopup";
+    
+    public static string EventPopupURL = "git@github.com:pinkfong-tech/com.thepinkfongcompany.ui.eventpopup.git";
+    public static string EventPopupPath = "MyPackages/EventPopup";
+    
+    public static string PurchasePopupURL = "git@github.com:pinkfong-tech/com.thepinkfongcompany.ui.purchasepopup.git";
+    public static string PurchasePopupPath = "MyPackages/PurchasePopup";
+    
+    public static string I2URL = "git@github.com:pinkfong-tech/com.thepinkfong.utility.i2.git";
+    public static string I2Path = "MyPackages/I2";
 }
--- Assets/Scripts/SDKManager.cs
+++ Assets/Scripts/SDKManager.cs
@@ -45,14 +45,14 @@ public class SDKManager : EditorWindow
         GUILayout.Space(20);
 
         #region Membership
-        GUILayout.Label("Membersh");
+        GUILayout.Label("Membership");
         GUILayout.BeginHorizontal();
         // 임포트 여부 갱신
-        _isImported = Directory.Exists(Path.Combine(Application.dataPath, PathConstant.MembershipPath));
+        _isImported = Directory.Exists(Path.Combine(Application.dataPath, PathConstant.MembershipPath)) && !CheckEmpty(PathConstant.MembershipPath);
 
         // 다운로드 및 임포트 버튼
         GUI.enabled = !_isImported; // 이미 임포트된 경우 버튼 비활성화
-        if (GUILayout.Button("Download and Import Logger"))
+        if (GUILayout.Button("Import Membership"))
         {
             CloneAndImportPackage(PathConstant.MembershipURL, PathConstant.MembershipPath);
         }
@@ -64,10 +64,153 @@ public class SDKManager : EditorWindow
             DeletePackage(PathConstant.MembershipPath);
         }
         GUILayout.EndHorizontal();
-        #endregion  // 멤버십
+        #endregion  // Membership
+        
+        GUILayout.Space(5);
+
+        #region OnePush
+        GUILayout.Label("OnePush");
+        GUILayout.BeginHorizontal();
+        _isImported = Directory.Exists(Path.Combine(Application.dataPath, PathConstant.OnePushPath)) && !CheckEmpty(PathConstant.MembershipPath);
+
+        GUI.enabled = !_isImported;
+        if (GUILayout.Button("Import OnePush"))
+        {
+            CloneAndImportPackage(PathConstant.OnePushURL, PathConstant.OnePushPath);
+        }
+
+        GUI.enabled = _isImported;
+        if (GUILayout.Button("Delete", GUILayout.Width(47)))
+        {
+            DeletePackage(PathConstant.OnePushPath);
+        }
+        GUILayout.EndHorizontal();
+        #endregion  // OnePush
+        
+        GUILayout.Space(5);
+
+        #region MobileNativePopup
+        GUILayout.Label("MobileNativePopup");
+        GUILayout.BeginHorizontal();
+        _isImported = Directory.Exists(Path.Combine(Application.dataPath, PathConstant.MobileNativePopupPath)) && !CheckEmpty(PathConstant.MembershipPath);
+
+        GUI.enabled = !_isImported; 
+        if (GUILayout.Button("Import MobileNativePopup"))
+        {
+            CloneAndImportPackage(PathConstant.MobileNativePopupURL, PathConstant.MobileNativePopupPath);
+        }
+        
+        GUI.enabled = _isImported; 
+        if (GUILayout.Button("Delete", GUILayout.Width(47)))
+        {
+            DeletePackage(PathConstant.MobileNativePopupPath);
+        }
+        GUILayout.EndHorizontal();
+        #endregion  // MobileNativePopup
+        
+        GUILayout.Space(5);
+
+        #region EventPopup
+        GUILayout.Label("EventPopup");
+        GUILayout.BeginHorizontal();
+        _isImported = Directory.Exists(Path.Combine(Application.dataPath, PathConstant.EventPopupPath)) && !CheckEmpty(PathConstant.MembershipPath);
+
+        GUI.enabled = !_isImported; 
+        if (GUILayout.Button("Import EventPopup"))
+        {
+            CloneAndImportPackage(PathConstant.EventPopupURL, PathConstant.EventPopupPath);
+        }
+        
+        GUI.enabled = _isImported; 
+        if (GUILayout.Button("Delete", GUILayout.Width(47)))
+        {
+            DeletePackage(PathConstant.EventPopupPath);
+        }
+        GUILayout.EndHorizontal();
+        #endregion  // EventPopup
+        
+        GUILayout.Space(5);
+
+        #region PurchasePopup
+        GUILayout.Label("PurchasePopup");
+        GUILayout.BeginHorizontal();
+        _isImported = Directory.Exists(Path.Combine(Application.dataPath, PathConstant.PurchasePopupPath)) && !CheckEmpty(PathConstant.MembershipPath);
+
+        GUI.enabled = !_isImported; 
+        if (GUILayout.Button("Import PurchasePopup"))
+        {
+            CloneAndImportPackage(PathConstant.PurchasePopupURL, PathConstant.PurchasePopupPath);
+        }
+        
+        GUI.enabled = _isImported; 
+        if (GUILayout.Button("Delete", GUILayout.Width(47)))
+        {
+            DeletePackage(PathConstant.PurchasePopupPath);
+        }
+        GUILayout.EndHorizontal();
+        #endregion  // PurchasePopup
+        
+        GUILayout.Space(5);
+   
+        #region I2
+        GUILayout.Label("I2");
+        GUILayout.BeginHorizontal();
+        _isImported = Directory.Exists(Path.Combine(Application.dataPath, PathConstant.I2Path)) && !CheckEmpty(PathConstant.I2Path);
+
+        GUI.enabled = !_isImported; 
+        if (GUILayout.Button("Import PurchasePopup"))
+        {
+            CloneAndImportPackage(PathConstant.I2URL, PathConstant.I2Path);
+        }
+        
+        GUI.enabled = _isImported; 
+        if (GUILayout.Button("Delete", GUILayout.Width(47)))
+        {
+            DeletePackage(PathConstant.I2Path);
+        }
+        GUILayout.EndHorizontal();
+        #endregion  // I2
         
         // 버튼 상태 초기화
         GUI.enabled = true;
+    }
+    
+    private static bool CheckForUpdates(string relativePath)
+    {
+        string absolutePath = Path.Combine(Application.dataPath, relativePath);
+        if (!Directory.Exists(absolutePath)) return false;
+        try
+        {
+            // Git fetch 실행
+            Process gitProcess = new Process();
+            gitProcess.StartInfo.FileName = "git";
+            gitProcess.StartInfo.Arguments = "fetch";
+            gitProcess.StartInfo.WorkingDirectory = absolutePath;
+            gitProcess.StartInfo.CreateNoWindow = true;
+            gitProcess.StartInfo.UseShellExecute = false;
+            gitProcess.StartInfo.RedirectStandardOutput = true;
+            gitProcess.StartInfo.RedirectStandardError = true;
+            gitProcess.Start();
+            gitProcess.WaitForExit();
+            // 로컬과 원격 브랜치 비교
+            Process statusProcess = new Process();
+            statusProcess.StartInfo.FileName = "git";
+            statusProcess.StartInfo.Arguments = "status -uno";
+            statusProcess.StartInfo.WorkingDirectory = absolutePath;
+            statusProcess.StartInfo.CreateNoWindow = true;
+            statusProcess.StartInfo.UseShellExecute = false;
+            statusProcess.StartInfo.RedirectStandardOutput = true;
+            statusProcess.StartInfo.RedirectStandardError = true;
+            statusProcess.Start();
+            string output = statusProcess.StandardOutput.ReadToEnd();
+            statusProcess.WaitForExit();
+            return output.Contains("behind"); // 원격 저장소와 비교하여 뒤처진 경우 업데이트 필요
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogError("Error checking for updates: " + ex.Message);
+            return false;
+        }
     }
 
     private static void CloneAndImportPackage(string url, string relativePath)
@@ -143,5 +286,11 @@ public class SDKManager : EditorWindow
         {
             Debug.LogError("Error deleting package: " + ex.Message);
         }
+    }
+
+    private static bool CheckEmpty(string relativePath)
+    {
+        var path = Path.Combine(Application.dataPath, relativePath);
+        return Directory.GetFiles(path).Length.Equals(0) && Directory.GetDirectories(path).Length.Equals(0);
     }
 }
